105733aa918a963b8a56e180c117b94affe208db
Yogesh Verma <yogesh_verma2001@hotmail.com>
2021-01-24 16:52:12 +0530

    Add some more JS

--- omnifood/resources/javascript/script.js
+++ omnifood/resources/javascript/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+    /* For navigation */
+
     $('.js--section-features').waypoint(function(direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
